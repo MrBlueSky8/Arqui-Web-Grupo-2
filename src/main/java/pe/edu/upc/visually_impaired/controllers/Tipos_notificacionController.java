223fedf73955fe3df94becb5fa1180d0f00c02ac
boke26 <117843716+boke26@users.noreply.github.com>
2024-04-26 23:00:20 -0500

    Privilegios de tipos de notificación / seguridad

--- src/main/java/pe/edu/upc/visually_impaired/controllers/Tipos_notificacionController.java
+++ src/main/java/pe/edu/upc/visually_impaired/controllers/Tipos_notificacionController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.visually_impaired.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.visually_impaired.dtos.Tipos_de_usuarioDTO;
 import pe.edu.upc.visually_impaired.dtos.Tipos_notificacionDTO;
@@ -18,12 +19,14 @@ public class Tipos_notificacionController {
     @Autowired
     private ITipos_notificacionService tiS;
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void insertar(@RequestBody Tipos_notificacionDTO tiposNotificacionDTO) {
         ModelMapper d=new ModelMapper();
         Tipos_notificacion tipos_notificacion = d.map(tiposNotificacionDTO,Tipos_notificacion.class);
         tiS.insert(tipos_notificacion);
     }
     @PutMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void modificar(@RequestBody Tipos_notificacionDTO tiposNotificacionDTO) {
         ModelMapper d=new ModelMapper();
         Tipos_notificacion tipos_notificacion = d.map(tiposNotificacionDTO,Tipos_notificacion.class);
