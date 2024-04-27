481d211779136a09629fe6cdee43327ace4d3234
boke26 <117843716+boke26@users.noreply.github.com>
2024-04-26 23:15:07 -0500

    Actualización Security HistorialNavegación

--- src/main/java/pe/edu/upc/visually_impaired/controllers/HistorialNavegacionController.java
+++ src/main/java/pe/edu/upc/visually_impaired/controllers/HistorialNavegacionController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.visually_impaired.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.visually_impaired.dtos.HistorialNavegacionDTO;
 import pe.edu.upc.visually_impaired.dtos.HistorialXFechaDTO;
@@ -23,18 +24,21 @@ public class HistorialNavegacionController {
     @Autowired
     private IHistorialNavegacionService hS;
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void insertar(@RequestBody HistorialNavegacionDTO historialNavegacionDTO){
         ModelMapper a = new ModelMapper();
         HistorialNavegacion historialNavegacion=a.map(historialNavegacionDTO,HistorialNavegacion.class);
         hS.insert(historialNavegacion);
     }
     @PutMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void modificar(@RequestBody HistorialNavegacionDTO historialNavegacionDTO) {
         ModelMapper d=new ModelMapper();
         HistorialNavegacion historialNavegacion = d.map(historialNavegacionDTO,HistorialNavegacion.class);
         hS.insert(historialNavegacion);
     }
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     public List<HistorialNavegacion> listar(){
         return hS.list().stream().map(y->{
             ModelMapper e = new ModelMapper();
@@ -43,10 +47,12 @@ public class HistorialNavegacionController {
         }).collect(Collectors.toList());
     }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void eliminar(@PathVariable("id") Integer id){
         hS.delete(id);
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
     public HistorialNavegacionDTO listarId(@PathVariable Integer id){
         ModelMapper m = new ModelMapper();
         HistorialNavegacionDTO dto=m.map(hS.listId(id),HistorialNavegacionDTO.class);
@@ -54,6 +60,7 @@ public class HistorialNavegacionController {
     }
 
     @GetMapping("/rutamasviajadas")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<RutaMasTransitadaDTO> Ruta_mas_transitada(){
         List<String[]> filaLista = hS.Ruta_mas_transitada();
         List<RutaMasTransitadaDTO> dtoLista=new ArrayList<>();
@@ -68,6 +75,7 @@ public class HistorialNavegacionController {
         return dtoLista;
     }
     @GetMapping("/historialporfecha")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<HistorialXFechaDTO> HistorialPorFecha(@RequestParam int usuario_id, @RequestParam LocalDate fechas){
         List<String[]> filaLista = hS.FiltrarHistorialXFecha(usuario_id, fechas);
         List<HistorialXFechaDTO> dtoLista=new ArrayList<>();
@@ -84,6 +92,7 @@ public class HistorialNavegacionController {
         return dtoLista;
     }
     @GetMapping("/historialporperiodo")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<HistorialXFechaDTO> HistorialPorPeriodo(@RequestParam int usuario_id, @RequestParam String periodo){
         List<String[]> filaLista = hS.HistorialNavegacion_Por_Periodo(usuario_id, periodo.toLowerCase());
         List<HistorialXFechaDTO> dtoLista=new ArrayList<>();
